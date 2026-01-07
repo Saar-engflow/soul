2b85de6250f4ea2822410fb5faf022e3264356d0
Raas-Lobby <robinsdev2@gmail.com>
2026-01-07 21:24:19 +0200

    add yaml

--- autonomous_agent/server.py
+++ autonomous_agent/server.py
@@ -90,4 +90,5 @@ if __name__ == '__main__':
     heartbeat_thread.start()
     
     # Run server
-    socketio.run(app, host='0.0.0.0', port=5000, debug=False)
+    port = int(os.environ.get('PORT', 5000))
+    socketio.run(app, host='0.0.0.0', port=port, debug=False)
